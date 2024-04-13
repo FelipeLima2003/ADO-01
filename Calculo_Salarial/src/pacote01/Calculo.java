cb623f322fc146ac194644e90305e86e4a8c1574
Felipe lima <140981807+FelipeLima2003@users.noreply.github.com>
2024-04-13 20:31:33 -0300

    Adicionando arquivo

---
+++ Calculo_Salarial/src/pacote01/Calculo.java
@@ -0,0 +1,13 @@
+
+package pacote01;
+
+
+public class Calculo {
+    public void Irpf (float salario){
+        if (salario <= 2.112){
+            System.out.print("O salário é: " +salario);
+        }
+        
+        else if (salario )
+    } 
+}
